b3515acfd04030b3b06c51ed8f526d03b85ca8ed
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-11 19:57:47 +0530

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -201,7 +201,7 @@ cmd({
             teks1 = `\n\n*User* : @${
     citel.sender.split("@")[0]
   }\n*Request* : ${text}`;
-            teks2 = `\n\n*Hii ${pushname},Your report has been forwarded to my Developer*.\n*Please wait.......*`;
+            teks2 = `\n\n*Hii ${citel.pushname},Your report has been forwarded to my Developer*.\n*Please wait.......*`;
             for (let i of owner) {
                 Void.sendMessage(i + "@s.whatsapp.net", {
                     text: textt + teks1,
