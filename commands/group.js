91f51e891468ccb116d0656b79c7e0c36815e998
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-11 20:53:33 +0530

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -9,7 +9,7 @@
  * @version 0.0.6
  **/
 
-const { sck, sck1,cmd, jsonformat, botpic, TelegraPh, RandomXP, Config, tlang, warndb, sleep,getAdmin,getBuffer, prefix, command } = require('../lib')
+const { sck, sck1,cmd, jsonformat, botpic, TelegraPh, RandomXP, Config, tlang, warndb, sleep,getAdmin,getBuffer, prefix, command, isCreator} = require('../lib')
 const moment = require("moment-timezone");
 const fs = require('fs-extra')
 const Levels = require("discord-xp");
@@ -871,53 +871,49 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
-        pattern: "broadcast",
-        alias: ["bc"],
-        desc: "Bot makes a broadcast in all groups",
-        fromMe: true,
-        category: "group",
-        filename: __filename,
-        use: '<text for broadcast.>',
-    },
-    async(Void, citel, text) => {
-        if (!isCreator) return citel.reply(tlang().owner)
-        let getGroups = await Void.groupFetchAllParticipating();
-        let groups = Object.entries(getGroups)
-            .slice(0)
-            .map((entry) => entry[1]);
-        let anu = groups.map((v) => v.id);
-        citel.reply(`Send Broadcast To ${anu.length} Group Chat, Finish Time ${
-          anu.length * 1.5
-        } second`);
-        for (let i of anu) {
-            await sleep(1500);
-            let txt = `*--❗${tlang().title} Broadcast❗--*\n\n *🍀Author:* ${citel.pushName}\n\n${text}`;
-            let buttonMessaged = {
-                image: log0,
-                caption: txt,
-                footer: citel.pushName,
-                headerType: 1,
-                contextInfo: {
-                    forwardingScore: 999,
-                    isForwarded: false,
-                    externalAdReply: {
-                        title: 'Broadcast by ' + citel.pushName,
-                        body: tlang().title,
-                        thumbnail: log0,
-                        mediaUrl: '',
-                        mediaType: 2,
-                        sourceUrl: gurl,
-                        showAdAttribution: true,
-                    },
+    pattern: "broadcast",
+    alias: ["bc"],
+    desc: "Bot makes a broadcast in all groups",
+    fromMe: true,
+    category: "group",
+    filename: __filename,
+    use: '<text for broadcast.>',
+},
+async (Void, citel, text) => {
+    if (!isCreator) return citel.reply(tlang().owner);
+    let getGroups = await Void.groupFetchAllParticipating();
+    let groups = Object.values(getGroups);
+    let anu = groups.map((v) => v.jid);
+    citel.reply(`Send Broadcast To ${anu.length} Group Chat, Finish Time ${anu.length * 1.5} second`);
+    for (let i of anu) {
+        await sleep(1500);
+        let txt = `*--❗${tlang().title} Broadcast❗--*\n\n *🍀Author:* ${citel.pushName}\n\n${text}`;
+        let buttonMessaged = {
+            imageMessage: {
+                url: log0
+            },
+            caption: txt,
+            footer: citel.pushName,
+            headerType: 1,
+            contextInfo: {
+                forwardingScore: 999,
+                isForwarded: false,
+                externalAdReply: {
+                    title: 'Broadcast by ' + citel.pushName,
+                    body: tlang().title,
+                    thumbnailUrl: log0,
+                    mediaType: 2,
+                    mediaUrl: '',
+                    sourceUrl: gurl,
+                    showAdAttribution: true,
                 },
-            };
-            await Void.sendMessage(i, buttonMessaged, {
-                quoted: citel,
-            });
-        }
-        citel.reply(`*Successful Sending Broadcast To ${anu.length} Group(s)*`);
+            },
+        };
+        await Void.sendMessage(i, buttonMessaged, MessageType.buttonsMessage, { quoted: citel });
     }
-)
+    citel.reply(`*Successful Sending Broadcast To ${anu.length} Group(s)*`);
+});
+        
 
 //---------------------------------------------------------------------------
 if(Config.WORKTYPE!=='private'){
