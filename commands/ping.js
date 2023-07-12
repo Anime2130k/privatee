a47b240e105c9c3d2ec2b45d53bcf2ce35c7cd81
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-12 08:25:08 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -13,6 +13,7 @@
 const Secktor = require('../lib')
 Secktor.cmd({
         pattern: "ping",
+ react: "⚡",
         desc: "To check ping",
         category: "general",
         filename: __filename,
