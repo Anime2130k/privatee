4e068f271afa5aacd9b3cef0edcf7d456d12bffb
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-12 07:54:29 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -24,7 +24,7 @@ Secktor.cmd({
             alias: ["menu","h"],
             desc: "Help list",
             category: "general",
-            react: "🌸",
+            react: "⛩️",
             filename: __filename
         },
         async(Void, citel, text) => {
