ad9c44a0fc39633e309ffac4ec6698dbcc140191
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-13 09:39:33 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,7 +1,7 @@
 const {cmd } = require('../lib')
 cmd({
-        pattern: "insta",
-        alias: "igdl",
+        pattern: "idhdhd",
+        alias: "igdrjdjdjl",
         desc: "download instagram post.",
         react: "🛐",
         category: "downloader",
@@ -9,7 +9,7 @@ cmd({
     },
     async(Void, citel,text,{isCreator}) => {
         const { Insta } = require('../lib')
-if(!text) return citel.reply('Need post url!.')
+if(!text) return citel.reply('No such command baka!!.')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
