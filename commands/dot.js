a3843664caf426bcaf8e3a8e6d2baf0f7c7b510e
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-13 09:35:25 +0530

    Update dot.js

--- commands/dot.js
+++ commands/dot.js
@@ -1,1 +1,17 @@
-
+const {cmd } = require('../lib')
+cmd({
+        pattern: "hfyhdhdjdjxhxjcjdgn",
+        alias: "kgjskghkxhmdkhxhihb",
+        desc: "download instagram post.",
+        react: "❌",
+        category: "downloader",
+        filename: __filename
+    },
+    async(Void, citel,text,{isCreator}) => {
+        const { Insta } = require('../lib')
+if(!text) return citel.reply('No suck command!.')
+let response = await Insta(text)
+for (let i=0;i<response.length;i++) {
+await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+}
+    });
