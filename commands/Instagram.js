2d8686d6a6e4e367b97a63fb22073238aa073ba0
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-14 13:59:11 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -8,6 +8,7 @@ cmd({
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
+            if(citel.isGroup) {
         const { Insta } = require('../lib')
 if(!text) return citel.reply('Mikasa is active! Type *.help* to get my command list!')
 let response = 'Mikasa is active! Type *.help* to get my command list!'
@@ -15,3 +16,8 @@ for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[i], `*Mikasa is active! Type *.help* to get my command list!*`, citel)
 }
     });
+} else {
+    // Reply with a warning for PMs
+    return await citel.reply("*⚠️ This bot does not accept commands in personal messages. Please use it in a group chat.*");
+  }
+});
