51e0abd0aa72ad47a5998bd3fc60b6ec71a064ca
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-07-14 14:52:40 +0530

    Update _plugins.js

--- commands/_plugins.js
+++ commands/_plugins.js
@@ -36,29 +36,25 @@ cmd({
 //---------------------------------------------------------------------------
 
 cmd({
-  pattern: "remove",
-  alias: ['uninstall'],
-  react: "🗿",
-  category: "owner",
-  desc: "removes external plugins.",
-  filename: __filename
-}, async (Void, citel, text, { isCreator }) => {
-  if (!isCreator) return citel.reply(tlang().owner);
-  if (text === 'all') {
-    if (citel.isGroup) {
-      await plugindb.collection.drop();
-      return citel.reply('Deleted all plugins from Secktor.');
+        pattern: "remove",
+        alias :['uninstall'],
+        category: "owner",
+        desc: "removes external plugins.",
+        filename: __filename
+    },
+    async(Void, citel, text,{ isCreator}) => {
+        if (!isCreator) return citel.reply(tlang().owner)
+        if(text==='all') {
+         await plugindb.collection.drop()
+         return citel.reply('Deleted all plugins from Secktor.')
+        }
+        let kill = await remove(text.split(" ")[0])
+        delete require.cache[require.resolve(__dirname+"/" + text + ".js")];
+        fs.unlinkSync(__dirname + "/" + text+ ".js");
+        return citel.reply(kill)
     }
-  }
-  let kill = await remove(text.split(" ")[0]);
-  delete require.cache[require.resolve(__dirname + "/" + text + ".js")];
-  fs.unlinkSync(__dirname + "/" + text + ".js");
-  return citel.reply(kill);
-});
-// Reply with a warning for PMs
-else {
-  return await citel.reply("*⚠️ This bot does not accept commands in personal messages. Please use it in a group chat.*");
-}
+)
+
 
 
 //---------------------------------------------------------------------------
