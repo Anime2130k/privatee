e6375562307fb030186f2598ad8bc2fa1a8a24e2
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-08-28 12:15:51 +0530

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -1759,7 +1759,7 @@ _3:- .group Info_
 //---------------------------------------------------------------------------
 if(Config.WORKTYPE!=='private'){
 cmd({ on: "text" }, async(Void, citel) => {
-    const randomXp = 8;
+    const randomXp = 50;
     let usrname = citel.pushName
     const hasLeveledUp = await Levels.appendXp(citel.sender, "RandomXP", randomXp);
     if (hasLeveledUp) {
