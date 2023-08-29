6b03709c418892b315d7ca24ef0d4c4b3856a728
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-08-29 22:00:24 +0530

    Update TicTacToe.js

--- commands/TicTacToe.js
+++ commands/TicTacToe.js
@@ -266,52 +266,66 @@ ${
   }
 );
 
-cmd({ pattern: "ship" ,react: "🤭", category: "fun" }, async(Void, citel, text) => {
-    const { tlang } = require('../lib')
-   if (!citel.isGroup) return citel.reply(tlang().group);
-   const groupMetadata = citel.isGroup ? await Void.groupMetadata(citel.chat).catch((e) => {}) : "";
-	 const participants = citel.isGroup ? await groupMetadata.participants : "";
-   let members = participants.map(u => u.id)
-   const percentage = Math.floor(Math.random() * 100)
+cmd({ pattern: "ship", react: "🤭", category: "fun" }, async (Void, citel, text) => {
+    const { tlang } = require('../lib');
+    if (!citel.isGroup) return citel.reply(tlang().group);
+
+    const groupMetadata = citel.isGroup ? await Void.groupMetadata(citel.chat).catch((e) => {}) : "";
+    const participants = citel.isGroup ? groupMetadata.participants : [];
+    const members = participants.map(u => u.id);
+    
+    const percentage = Math.floor(Math.random() * 100);
+
     async function couple(percent) {
-         var text;
+        let text;
         if (percent < 25) {
-            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\tThere's still time to reconsider your choices`
+            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\tThere's still time to reconsider your choices`;
         } else if (percent < 50) {
-            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\t Good enough, I guess! 💫`
+            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\t Good enough, I guess! 💫`;
         } else if (percent < 75) {
-            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\t\tStay together and you'll find a way ⭐️`
+            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\t\t\tStay together and you'll find a way ⭐️`;
         } else if (percent < 90) {
-            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\tAmazing! You two will be a good couple 💖 `
+            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\tAmazing! You two will be a good couple 💖 `;
         } else {
-            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\tYou two are fated to be together 💙`
+            text = `\t\t\t\t\t*ShipCent : ${percentage}%* \n\tYou two are fated to be together 💙`;
         }
-        return text
-        }
-       var user = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
-       var shiper;
-       if (user) {
-       shiper = user
-       } else {
-       shiper = members[Math.floor(Math.random() * members.length)]
-       }
-       let caption = `\t❣️ *Matchmaking...* ❣️ \n`
-        caption += `\t\t✯────────────────────💘\n`
-        caption += `@${citel.sender.split('@')[0]}  x  @${shiper.split('@')[0]}\n`
-        caption += `\t\t✯────────────────────💘\n`
-        caption += await couple(percentage)
-        if(citel.sender.split('@')[0]===shiper.split('@')[0]) return citel.reply('```'+'Wait... What!!!,You wanna do matchmaking with yourself'+'```')
-        
-   let ter = (citel.chat,{text: caption,mentions: [citel.sender,shiper]},{quoted:citel});
-    let buttonMessaged = {
+        return text;
+    }
+
+    const user = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
+    let shiper;
+    if (user) {
+        shiper = user;
+    } else {
+        shiper = members[Math.floor(Math.random() * members.length)];
+    }
+
+    if (citel.sender.split('@')[0] === shiper.split('@')[0]) {
+        return citel.reply('```' + 'Wait... What!!!,You wanna do matchmaking with yourself' + '```');
+    }
+
+    const caption = `\t❣️ *Matchmaking...* ❣️ \n`
+        + `\t\t✯────────────────────💘\n`
+        + `@${citel.sender.split('@')[0]}  x  @${shiper.split('@')[0]}\n`
+        + `\t\t✯────────────────────💘\n`
+        + await couple(percentage);
+
+    const ter = {
+        text: caption,
+        mentions: [citel.sender, shiper],
+        quoted: citel
+    };
+
+    const buttonMessaged = {
         image: { url: await botpic() },
         caption: ter,
     };
-return await Void.sendMessage(citel.chat, buttonMessaged, {
-        quoted: citel,	  
-    });
 
+    return await Void.sendMessage(citel.chat, buttonMessaged, {
+        quoted: citel,
     });
+});
+
 
 const axios = require('axios');
 const fs = require('fs');
