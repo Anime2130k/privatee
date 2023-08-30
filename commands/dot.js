e72d86467b13323a3caaa6c6a51f1f8b03711e09
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-08-30 20:56:45 +0530

    Update dot.js

--- commands/dot.js
+++ commands/dot.js
@@ -63,3 +63,35 @@ for (let i=0;i<response.length;i++) {
 await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
 }
     });
+
+     cmd({
+        pattern: "checkmarried",
+        alias: "ig",
+        desc: "download instagram post.",
+        react: "🛐",
+        category: "downloader",
+        filename: __filename
+    },
+    async(Void, citel,text,{isCreator}) => {
+            if (!citel.isGroup && !isCreator) return citel.reply(`*⚠️WARNING⚠️*
+  
+_Don't text the Bot in pm._
+
+*㊙️If you want to add this bot in your GC(GroupChat) then ask the Developer*
+
+*〽️Developer:*
+• wa.me/916000530073
+
+*📛Our official Support GC: http://surl.li/eumln*
+
+*💗Have a nice day💗*
+
+_Mikasa💗 Powered by:_ *©️RONEN-BOTS*
+`);
+        const { checkmarried } = require('../lib')
+if(!text) return citel.reply('Need Username.')
+let response = await checkmarried(text)
+for (let i=0;i<response.length;i++) {
+await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+}
+    });
