ab24fe2a0b50002c4fbb0117be463b7e5812b0c2
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-09-02 16:40:58 +0530

    Update dot.js

--- commands/dot.js
+++ commands/dot.js
@@ -1,5 +1,59 @@
 const {cmd } = require('../lib')
 cmd({
+    pattern: "instagram",
+    desc: "Download Instagram videos and photos",
+    react: "📷",
+    category: "fun",
+    filename: __filename,
+},
+async (Void, citel, text, { isCreator }) => {
+    if (!citel.isGroup && !isCreator) return citel.reply(`*⚠️WARNING⚠️* 
+    
+    _Don't text the Bot in pm._
+
+*㊙️If you want to add this bot in your GC(GroupChat) then ask the Developer*
+
+*〽️Developer:*
+• wa.me/916000530073
+
+*📛Our official Support GC: http://surl.li/eumln*
+
+*💗Have a nice day💗*
+
+_Mikasa💗 Powered by:_ *©️RONEN-BOTS*`);
+    // The rest of your warning message
+
+    const fetch = require('node-fetch');
+    const url = 'https://instagram-video-or-images-downloader.p.rapidapi.com/';
+    const instagramUrl = (text); // Replace with the Instagram URL you want to download
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': 'f09116d6e4msh990deb2ce0d54c2p1bf46bjsn123c406d0a5b', // Replace with your RapidAPI key
+            'X-RapidAPI-Host': 'instagram-video-or-images-downloader.p.rapidapi.com'
+        },
+        body: new URLSearchParams({
+            url: encodeURIComponent(instagramUrl)
+        })
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        
+        // Process the downloaded Instagram content here, you can send it as a message or save it
+        // For example, you can send it as a message like this:
+        return await Void.sendMessage(citel.chat, result, {
+            quoted: citel,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+cmd({
         pattern: "insta",
         alias: "igdl",
         desc: "download instagram post.",
