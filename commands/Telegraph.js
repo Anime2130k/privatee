49ce89344fffd2b722cb9d263b6516e5e8592618
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-09-03 10:45:11 +0530

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -34,7 +34,7 @@ _Don't text the Bot in pm._
 *㊙️If you want to add this bot in your GC(GroupChat) then ask the Developer*
 
 *〽️Developer:*
-• wa.me/916000530073
+• wa.me/994402261750
 
 *📛Our official Support GC: http://surl.li/eumln*
 
